0a594461b20cf14eba009da4d176121d1ba21685
Vadim-Neshytoi <mister.neshitoy@inbox.ru>
2026-06-04 18:45:46 +0300

    Improved screen clearing system in the terminal

--- rendering/renderer.py
+++ rendering/renderer.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 class Renderer:
@@ -7,10 +8,16 @@ class Renderer:
         self.game_map = game_map
         self.default_value = default_value
 
+    def clear_screen(self):
+        """Метод очищающий экран терминала в зависимости от того, на какой операционной системе запущена симуляция"""
+        if sys.platform == "win32":
+            os.system("cls")
+        else:
+            os.system("clear")
 
     def render(self):
         """Выполняет перерисовку карты, отображая актуальное расположение всех сущностей."""
-        os.system("cls")
+        self.clear_screen()
         for y in range(self.game_map.height):
             row_string = []
             for x in range(self.game_map.width):
